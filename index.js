ca83ddc7600de606455e6bffc375207ef8237a2e
Thierry Dequin <thierrydequin@hotmail.com>
2018-06-30 21:56:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // write your code below!
 var name = "Joe"
 var height = 74
-var message = ` is ${height} inches tall`
+var message = `${name} is ${height} inches tall`
+
+name = "Susan"
+height = 36
 
 module.exports = {name, height, message}
